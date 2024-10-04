d02f638aa4ce7768cba2edcc246a57c79c3e74f4
sandeepawasthi8878 <sandeepawasthi8878@gmail.com>
2024-10-03 23:31:51 -0700

    fifthcommit

--- src/Info/About.jsx
+++ src/Info/About.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FaFilm, FaInfoCircle } from 'react-icons/fa';
 
 function About() {
   return (
 
      <div className="font-semibold  bg-zinc-800 m-[5%] p-[5%] rounded-lg shadow-2xl max-w-lg mx-auto text-center">
       <h1 className="text-4xl font-bold text-white mb-4 flex items-center justify-center">
-        <FaInfoCircle className="mr-2" /> About Us
+        About Us
       </h1>
       <p className="text-white leading-relaxed mb-4">
         We provide detailed information about your favorite <span className="text-white font-semibold">movies</span>, 
@@ -16,7 +15,7 @@ function About() {
         Explore the latest trends, recommendations, and upcoming releases with a seamless and user-friendly interface.
       </p>
       <p className="text-white leading-relaxed">
-        <FaFilm className="inline-block mr-1 " /> Enjoy the world of cinema with us!
+        Enjoy the world of cinema with us!
       </p>
     </div>
   );

77e0374425d6b42c80c8d17c99920444c586492b
sandeepawasthi8878 <sandeepawasthi8878@gmail.com>
2024-10-03 23:18:57 -0700

    fourth commit

--- src/Info/About.jsx
+++ src/Info/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFilm, FaInfoCircle } from 'react-icons/fa';  // Importing React Icons
+import { FaFilm, FaInfoCircle } from 'react-icons/fa';
 
 function About() {
   return (
